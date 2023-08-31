b49e38cb7411fbcaa82ffc52ea5aea6c904f1846
engineerErica <ericaponce2002@gmail.com>
2023-08-31 20:21:46 +0000

    faves rough working

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ const randomFacts = 'https://dog-api.kinduff.com/api/facts';
 //DOM MANIPULATION FOR DISPLAYING DOG PIC
 let figure;
 const createDogPics = (imgURL) => {
-    figure = document.querySelector('figure');
-    const img = document.createElement('img');
-    img.src = imgURL;
-    figure.append(img)
+  figure = document.querySelector('figure');
+  const img = document.createElement('img');
+  img.src = imgURL;
+  figure.append(img)
 }
 //DOM MANIPULATION FOR DROP DOWN ADDING EACH BREED
 const createDogBreedOptions = (breed) => {
@@ -21,25 +21,25 @@ const createDogBreedOptions = (breed) => {
 }
 
 // DOM MANIPULATION FOR DISPLAYING DOG FACTS
- let figCap;
- const createDogFacts = (facts) => {
+let figCap;
+const createDogFacts = (facts) => {
   figCap = document.querySelector('figcaption');
   const p = document.createElement('p');
   p.textContent = facts
   figCap.append(p)
-   
- }
+
+}
 
 //FETCHING THE RANDOM DOG PICS FROM API
 const generateRandomDogPic = async (url) => {
-    try {
-        const response = await fetch(url);
-        const dogs = await response.json();
-        createDogPics(dogs.message);
-    }
-    catch (err) {
-        console.error(err)
-    }
+  try {
+    const response = await fetch(url);
+    const dogs = await response.json();
+    createDogPics(dogs.message);
+  }
+  catch (err) {
+    console.error(err)
+  }
 }
 
 ///FETCHING RANDOM DOG FACTS FROM API
@@ -62,7 +62,8 @@ const listBreeds = async (url) => {
     for (const breed in breeds.message) {
       createDogBreedOptions(breed)
     }
-  } catch (err) {
+  }
+  catch (err) {
     console.error(err);
   }
 }
@@ -81,7 +82,8 @@ function filterFunction() {
     let txtValue = a[i].textContent || a[i].innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
       a[i].style.display = "";
-    } else {
+    }
+    else {
       a[i].style.display = "none";
     }
   }
@@ -91,22 +93,80 @@ listBreeds(breedList)
 generateRandomDogPic(randomDogPic);
 generateRandomDogFacts(randomFacts);
 
+//CREATING NEW ITEMS IN LOCAL STORAGE
+const faveImgs = [];
+const saveImg = (idx, imgURL) => {
+  localStorage.setItem(idx, imgURL);
+}
+
+//GET ITEMS FROM LOCAL STORAGE
+// const getImg = (img) => {
+//   localStorage.getItem()
+// }
+
+//DOM MANIPULATION FOR DISPLAYING FAVORITES
+const displayFaves = () => {
+  const favesEl = document.querySelector('#faves');
+  favesEl.innerHTML = '';
+  console.log(faveImgs)
+  getFaveImgs().forEach((imgURL) => {
+    const img = document.createElement('img');
+    img.src = imgURL;
+    favesEl.append(img);
+  })
+  // const oldImg = document.querySelector('img').remove()
+  
+}
+//getFaveImgs and saveNewFaveImgs
+const getFaveImgs = () => {
+  // console.log(arr);
+  // arr.forEach((img) => {
+  //   displayFaves(img);
+  // })
+  return faveImgs;
+}
+
+const saveNewFaveImgs = (url) => {
+  if (!faveImgs.includes(url)) faveImgs.push(url);
+  console.log(faveImgs)
+}
+
 //EVENT LISTENERS
 
-document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('#btn').addEventListener('click', (e) => {
-    generateRandomDogPic(randomDogPic);
+document.querySelector('#viewFaves').addEventListener('click', (e) => {
+  displayFaves();
+  document.querySelector('#favPopUp').showModal()
+  // console.log(e.target, document.querySelector('img'))
+  // for (let i = 0; i < faveImgs.length; i++) {
+    // console.log(localStorage.getItem(i))
+    console.log(faveImgs)
+  // }
+});
+// localStorage.clear()
+document.querySelector('#favBtn').addEventListener('click', (e) => {
+//   console.log(e.target)
+  const imgURL = document.querySelector('img').src;
+  // if (!faveImgs.includes(imgURL)) faveImgs.push(imgURL)
+  // saveImg(faveImgs.indexOf(imgURL).toString(), imgURL)
+  // console.log(localStorage)
+  saveNewFaveImgs(imgURL);
+  console.log(faveImgs);
+});
+
+document.querySelector('#btn').addEventListener('click', (e) => {
+  figure.innerHTML = ''
+  generateRandomDogPic(randomDogPic);
+  // generateRandomDogFacts(randomFacts)
+});
+
+document.querySelector("#myDropdown").addEventListener('click', async (e) => {
+  try {
+    const response = await fetch(`https://dog.ceo/api/breed/${e.target.dataset.breed}/images/random`);
+    const selectedBreed = await response.json();
     figure.innerHTML = ''
-  });
-  document.querySelector("#myDropdown").addEventListener('click', async (e) => {
-    try {
-      const response = await fetch(`https://dog.ceo/api/breed/${e.target.dataset.breed}/images/random`);
-      const selectedBreed = await response.json();
-      figure.innerHTML = ''
-      createDogPics(selectedBreed.message)
-    } catch (err) {
-      console.error(err);
-    }
-    // console.log(e.target.data-breed)
-  })
+    createDogPics(selectedBreed.message)
+  }
+  catch (err) {
+    console.error(err);
+  }
 });
